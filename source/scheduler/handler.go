abe4d32f6f9555cd39492d82cee888a9752235e7
Matteo Ridolfi <matteo.ridolfi@danielwellington.com>
2019-03-25 10:32:26 +0100

    shouldRun logic

---
+++ source/scheduler/handler.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+var scheduleTag = os.Getenv("SCHEDULE_TAG")
+var scheduleTagDay = os.Getenv("SCHEDULE_TAG_DAY")
+
+type scheduler struct {
+	instanceID    string
+	instanceState ec2.InstanceStateName
+	startTime     time.Time
+	stopTime      time.Time
+	weekdays      []time.Weekday
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler() error {
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return err
+	}
+	client := ec2.New(cfg)
+
+	resp, err := client.DescribeInstancesRequest(&ec2.DescribeInstancesInput{
+		Filters: []ec2.Filter{
+			{
+				Name: aws.String("instance-state-name"),
+				Values: []string{
+					"running",
+					"stopped",
+				},
+			},
+			{
+				Name: aws.String("tag-key"),
+				Values: []string{
+					scheduleTag,
+				},
+			},
+		},
+	}).Send()
+	if err != nil {
+		return err
+	}
+
+	if len(resp.Reservations) < 1 {
+		log.Printf("no scheduled instance found")
+		return nil
+	}
+
+	for _, instance := range resp.Reservations[0].Instances {
+		s := &scheduler{}
+		s.instanceID = *instance.InstanceId
+		s.instanceState = instance.State.Name
+
+		for _, tag := range instance.Tags {
+			// scheduler disabled
+			if *tag.Key == scheduleTag && strings.Contains(*tag.Value, "#") {
+				log.Printf("scheduler is disabled for instanceId %s", *instance.InstanceId)
+				break
+			}
+
+			if *tag.Key == scheduleTag {
+				startStopTime := strings.Split(*tag.Value, "-")
+				s.startTime, _ = time.Parse("15:04", startStopTime[0])
+				s.stopTime, _ = time.Parse("15:04", startStopTime[1])
+			}
+
+			if *tag.Key == scheduleTagDay {
+				err := json.Unmarshal([]byte(*tag.Value), &s.weekdays)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// splitting time and date logic
+// dateNow contains information regarding current date and time
+// timeNow contains information regarding current time (null value for YYYY, mm, dd)
+func (s *scheduler) shouldRun(dateNow, timeNow time.Time) ec2.InstanceStateName {
+	// should not run today
+	log.Printf("weekday: %s", dateNow.Weekday())
+	if !s.shouldRunDay(dateNow.Weekday()) {
+		log.Printf("%s should run today: false", s.instanceID)
+		return ec2.InstanceStateNameStopped
+	}
+
+	// debugging
+	log.Printf("time now: %s", timeNow)
+	log.Printf("%s start time: %s", s.instanceID, s.startTime)
+	log.Printf("%s stop time: %s", s.instanceID, s.stopTime)
+
+	// startTime-stopTime same day (07:00-19:30)
+	if s.startTime.Before(s.stopTime) {
+		if timeNow.After(s.startTime) && timeNow.Before(s.stopTime) {
+			return ec2.InstanceStateNameRunning
+		}
+
+		return ec2.InstanceStateNameStopped
+	}
+
+	// startTime-stopTime between days (22:00-03:00 = 22:00-23:59,00:00-03:00)
+	if timeNow.After(s.startTime) && timeNow.Before(time.Date(0000, 01, 01, 23, 59, 00, 00, time.UTC)) {
+		return ec2.InstanceStateNameRunning
+	}
+	if timeNow.After(time.Date(0000, 01, 01, 00, 00, 00, 00, time.UTC)) && timeNow.Before(s.stopTime) {
+		return ec2.InstanceStateNameRunning
+	}
+
+	return ec2.InstanceStateNameStopped
+}
+
+// check if instance should run based on day of the week
+func (s *scheduler) shouldRunDay(weekday time.Weekday) bool {
+	// by default run weekdays (1,2,3,4,5)
+	if len(s.weekdays) == 0 {
+		if weekday != 0 && weekday != 6 {
+			return true
+		}
+	}
+
+	for _, w := range s.weekdays {
+		if w == weekday {
+			return true
+		}
+	}
+
+	return false
+}
