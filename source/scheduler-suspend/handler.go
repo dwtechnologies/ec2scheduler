f2c091e1bba40968bb53bf0420206baaf95ed23d
Matteo Ridolfi <matteo.ridolfi@danielwellington.com>
2019-04-02 19:49:42 +0200

    return when tag is found. verify input parameters

--- source/scheduler-suspend/handler.go
+++ source/scheduler-suspend/handler.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -23,12 +24,30 @@ type inputEvent struct {
 
 var scheduleTag = os.Getenv("SCHEDULE_TAG")
 var scheduleTagSuspend = os.Getenv("SCHEDULE_TAG_SUSPEND")
+var scheduleTagSuspendLayouts = map[int]string{
+	4:  "2006",
+	6:  "200601",
+	8:  "20060102",
+	11: "20060102T15",
+	14: "20060102T15:04",
+}
 
 func main() {
 	lambda.Start(handler)
 }
 
 func handler(event inputEvent) (string, error) {
+	// parse suspend time
+	if _, ok := scheduleTagSuspendLayouts[len(event.UnsuspendDatetime)]; !ok {
+		log.Printf("[%s] layout doesn't match any supported one %s", event.InstanceID, event.UnsuspendDatetime)
+		return fmt.Sprintf("unable to parse date: %s", event.UnsuspendDatetime), nil
+	}
+	_, err := time.Parse(scheduleTagSuspendLayouts[len(event.UnsuspendDatetime)], event.UnsuspendDatetime)
+	if err != nil {
+		log.Printf("[%s] can't parse date %s", event.InstanceID, event.UnsuspendDatetime)
+		return fmt.Sprintf("unable to parse date: %s", event.UnsuspendDatetime), nil
+	}
+
 	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
 		return "", err
@@ -43,8 +62,8 @@ func handler(event inputEvent) (string, error) {
 	}
 
 	if len(resp.Reservations) < 1 {
-		log.Printf("no instance found %s", event.InstanceID)
-		return "", nil
+		log.Printf("[%s] no instance found", event.InstanceID)
+		return fmt.Sprintf("no instance found with ID %s", event.InstanceID), nil
 	}
 
 	for _, tag := range resp.Reservations[0].Instances[0].Tags {
@@ -63,13 +82,13 @@ func handler(event inputEvent) (string, error) {
 				return "", err
 			}
 
-			break
+			log.Printf("[%s] scheduler suspended until %s", event.InstanceID, event.UnsuspendDatetime)
+			return fmt.Sprintf("instance %s scheduler suspended until %s", event.InstanceID, event.UnsuspendDatetime), nil
 		}
-
 	}
 
-	log.Printf("instance %s scheduler suspended until %s", event.InstanceID, event.UnsuspendDatetime)
-	return fmt.Sprintf("instance %s scheduler suspended until %s", event.InstanceID, event.UnsuspendDatetime), nil
+	log.Printf("[%s] unable to find %s tag", event.InstanceID, scheduleTag)
+	return fmt.Sprintf("unable to find %s tag for instance %s", scheduleTag, event.InstanceID), nil
 }
 
 func createTags(client ec2iface.EC2API, instanceID string, tags []ec2.Tag) error {
